1f5a4e281f630c5dc066fb8a415ce1435c59e00c
songmin <tft0720@gmail.com>
2022-01-29 11:22:53 +0900

    달력 클릭 날짜 변환

--- src/components/SearchCalendar.tsx
+++ src/components/SearchCalendar.tsx
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
-import { DateRange, Preview, Range, RangeKeyDict } from "react-date-range";
-import styled from "styled-components";
-import "react-date-range/dist/styles.css";
-import "react-date-range/dist/theme/default.css";
+import React, { useState } from 'react';
+import { DateRange, Range, RangeKeyDict } from 'react-date-range';
+import styled from 'styled-components';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 const SearchCalendar = () => {
   const [selectedState, setSelectedState] = useState<Range[]>([
     {
       startDate: new Date(),
       endDate: new Date(),
-      key: "selection",
+      key: 'selection',
     },
   ]);
 
   const handleChange = (rangesByKey: RangeKeyDict) => {
     setSelectedState([rangesByKey.selection]);
-    console.log(rangesByKey);
+    const {
+      selection: { startDate: _startDate, endDate: _endDate },
+    } = rangesByKey;
+    const _startTemp = _startDate?.toLocaleDateString().replaceAll(' ', '').split('.')!;
+    const _endTemp = _endDate?.toLocaleDateString().replaceAll(' ', '').split('.')!;
+    const [startYear, startMonth, startDay] = _startTemp;
+    const [endYear, endMonth, endtDay] = _endTemp;
+    const startDate = `${startYear.trim()}.${startMonth.trim().padStart(2, '0')}.${startDay.trim().padStart(2, '0')}`;
+    const endDate = `${endYear.trim()}.${endMonth.trim().padStart(2, '0')}.${endtDay.trim().padStart(2, '0')}`;
+    console.log(startDate, endDate);
   };
 
   return (
@@ -25,6 +34,7 @@ const SearchCalendar = () => {
         onChange={handleChange}
         moveRangeOnFirstSelection={false}
         ranges={selectedState}
+        rangeColors={['red', 'blue']}
       />
     </Container>
   );
@@ -35,4 +45,13 @@ export default SearchCalendar;
 const Container = styled.div`
   text-align: center;
   position: absolute;
+  right: 60px;
+  top: 50px;
+  z-index: 20;
+  .rdrDay {
+    height: 3.5em;
+  }
+  .rdrMonth {
+    border-radius: 10px;
+  }
 `;
